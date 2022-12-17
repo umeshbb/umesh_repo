f04418871d48e791fa350a8745cf2b5e910e2c89
umeshbb <119994076+umeshbb@users.noreply.github.com>
2022-12-17 22:17:23 +0530

    Update Base.java

--- src/main/java/neostox/Base.java
+++ src/main/java/neostox/Base.java
@@ -9,8 +9,6 @@ public class Base {
 	protected  static WebDriver driver;
 	public void launchbrowser() throws IOException
 	{
-		
-		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Umesh\\Desktop\\selenium\\chromedriver.exe");
 		 driver=new ChromeDriver();
 		 driver.manage().window().maximize();
